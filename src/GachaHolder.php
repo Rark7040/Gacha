3cd54d29a70b10c643cf3a14d060846a61a76887
Rark7040 <crimera.hello@gmail.com>
2022-07-12 23:17:08 +0900

    added instance holder

---
+++ src/GachaHolder.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace rarkhopper\gacha;
+
+class GachaHolder{
+	/** @var array<int, Gacha> */
+	protected array $gacha_list = [];
+
+	public function create(string $title, ):void{
+
+	}
+}
