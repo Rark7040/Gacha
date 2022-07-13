67478d98335a11d4917b430c4ee5366893888004
Rark7040 <crimera.hello@gmail.com>
2022-07-13 09:10:53 +0900

    added IRarity.php

--- src/IGachaItem.php
+++ src/IGachaItem.php
@@ -6,7 +6,6 @@ namespace rarkhopper\gacha;
 use pocketmine\player\Player;
 
 interface IGachaItem{
-	/** @return float 0.0 ~ 100.0 */
-	public function getEmissionPercent():float; //TODO validate
+	public function getRarity():IRarity;
 	public function giveItem(Player $player):void;
 }
