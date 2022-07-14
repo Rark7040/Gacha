758190d671a4e265440f90cbb13f49fa7db77b9a
Rark7040 <crimera.hello@gmail.com>
2022-07-15 00:04:52 +0900

    gacha construct

--- src/Gacha.php
+++ src/Gacha.php
@@ -4,5 +4,15 @@ declare(strict_types=1);
 namespace rarkhopper\gacha;
 
 class Gacha{
+	protected string $name;
+	protected string $description;
+	protected ItemTable $table;
+	protected ITicket $ticket;
 
+	public function __construct(string $name, string $description, ItemTable $table, ITicket $ticket){
+		$this->name = $name;
+		$this->description = $description;
+		$this->table = $table;
+		$this->ticket = $ticket;
+	}
 }
